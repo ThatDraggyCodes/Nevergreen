c241e238522d6db3708b75f2f5d176e5eea6ff58
Dan Gough <goughdj@gmail.com>
2024-08-17 23:46:53 +0100

    Tweaks before merging

--- Nevergreen/Apps/Get-RedstorBackupProStoragePlatformConsole.ps1
+++ Nevergreen/Apps/Get-RedstorBackupProStoragePlatformConsole.ps1
@@ -1,13 +1,7 @@
 $UserAgents = @(
-    $Null,  # No user agent
-    # User agent for Edge, taken from the Get-Link example.
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
-    # Chrome 123.0, Windows
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.3',
-    # Edge 123.0, Windows
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.',
-    # Opera 108.0, Windows
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 OPR/108.0.0.'
+    $Null
+    # User agent from [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome in PowerShell 7
+    'Mozilla/5.0 (Windows NT 10.0; Microsoft Windows 10.0.22631; en-GB) AppleWebKit/534.6 (KHTML, like Gecko) Chrome/7.0.500.0 Safari/534.6'
 )
 $DownloadPageURL = $Null
 
@@ -31,7 +25,8 @@ foreach ($UserAgent in $UserAgents) {
 }
 
 if (-not $DownloadPageURL) {
-    Write-Error 'Could not convince the redstor website to skip the challange.'
+    Write-Error 'Could not connect to Redstor download page.'
+    return
 }
 
 $URL32 = Get-Link `
